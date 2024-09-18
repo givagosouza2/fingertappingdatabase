8dc90c24d3bc67c0ef09881b9d6c6e0acb3e629a
givagosouza2 <147889314+givagosouza2@users.noreply.github.com>
2024-09-18 10:33:43 -0300

    Update baseDados.py

--- baseDados.py
+++ baseDados.py
@@ -362,8 +362,8 @@ with tab2:
                     x_coordenadas, y_coordenadas)
                 ellipse_area = np.pi * ellipse_semiminor_axis * ellipse_semimajor_axis
                 features_area.append(ellipse_area)
-                x_squared = [a**2 for a in x_coordinadas]
-                y_squared = [b**2 for b in y_coordinadas]
+                x_squared = [a**2 for a in x_coordenadas]
+                y_squared = [b**2 for b in y_coordenadas]
                 total_deviation = np.sum(np.sqrt(x_squared+y_squared))
                 
                 features_deviation.append(total_deviation)
